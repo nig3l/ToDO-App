049db2ea49769f316a0e08771143523ab7ab509c
nigel <nigelchimwene@gmail.com>
2023-06-20 15:05:04 +0300

    startapp

--- ToDo_list/base/models.py
+++ ToDo_list/base/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import user
 
 # Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(user, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    complete = models.BooleanField(default=False)
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['complete']
+        
+
--- ToDo_list/base/views.py
+++ ToDo_list/base/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def Taskview(request):
+    return HttpResponse ('To Do List')
+
