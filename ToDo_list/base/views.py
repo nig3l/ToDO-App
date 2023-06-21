f46b75263d706e36ffb7167839b850bbf98b9cf4
nigel <nigelchimwene@gmail.com>
2023-06-21 14:27:51 +0300

    templates

--- ToDo_list/base/views.py
+++ ToDo_list/base/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 from .models import Task
 
 # Create your views here.
 
 class TaskList(ListView):
     model = Task
+    context_object_name = 'task'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = ''
     
 
