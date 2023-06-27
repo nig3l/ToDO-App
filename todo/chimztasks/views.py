561af512ee37be7831294f99543d0f60feee2f7f
nigel <nigelchimwene@gmail.com>
2023-06-27 13:44:42 +0300

    models

--- todo/chimztasks/views.py
+++ todo/chimztasks/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+from . models import *
 
 # Create your views here.
 
 def index(request):
-    return render(request , 'chimztasks/list.html')
+
+    tasks = Task.objects.all()
+    context = {'tasks':tasks}
+
+    return render(request , 'chimztasks/list.html', context)
