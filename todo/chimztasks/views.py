cb4a75d09fbfe65c85064a1943b9d647ac9285ef
nigel <nigelchimwene@gmail.com>
2023-06-27 14:13:48 +0300

    forms

--- todo/chimztasks/views.py
+++ todo/chimztasks/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+
+
 from . models import *
+from .forms import *
 
 # Create your views here.
 
 def index(request):
 
     tasks = Task.objects.all()
-    context = {'tasks':tasks}
+    form = TaskForm()
+
+    context = {'tasks':tasks , 'form':form}
 
     return render(request , 'chimztasks/list.html', context)
