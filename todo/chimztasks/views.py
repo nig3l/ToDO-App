8ff7ecbbcf655192ddacacbb6dd7c8e766d794f4
nigel <nigelchimwene@gmail.com>
2023-06-28 02:12:16 +0300

    form

--- todo/chimztasks/views.py
+++ todo/chimztasks/views.py
@@ -29,5 +29,19 @@ def index(request):
 def updateTask(request,pk):
    
    task = Task.objects.get(id=pk)
+
+   form = TaskForm(instance=task)
+
+   if request.method == 'POST':
+      form = TaskForm(request.POST , instance=task)
+      if form.is_valid():
+          form.save()
+
+          return redirect('/')
+
+      
+
+
+   context = {'form':form}
    
-   return render(request,'chimztasks/update_task.html')
+   return render(request,'chimztasks/update_task.html',context)
