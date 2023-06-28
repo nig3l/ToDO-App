347b18cab9c4ec2ea216bf49c435c916e7389e0e
nigel <nigelchimwene@gmail.com>
2023-06-28 13:02:06 +0300

    styling

--- todo/chimztasks/views.py
+++ todo/chimztasks/views.py
@@ -50,6 +50,11 @@ def updateTask(request,pk):
 def deleteTask(request,pk):
    item = Task.objects.get(id=pk)
 
+   if request.method == 'POST':
+      item.delete()
+      
+      return redirect('/')
+
    context = {'item':item}
 
    
